49493777abf3acf9020798095b0f26b52436961a
Kenneth Daily <kenneth.daily@sagebase.org>
2015-02-09 15:53:07 -0800

    added processing for HBTRC expression array

---
+++ Mount-Sinai/processHBTRCexpressionarray.R
@@ -0,0 +1,65 @@
+library(synapseClient)
+library(plyr)
+library(dplyr)
+library(reshape2)
+
+synapseLogin()
+
+## Get files
+alzfile <- synGet("syn2706445")
+normfile <- synGet("syn2706448")
+metafile <- synGet("syn3157400")
+
+## Load files
+alzdata <- read.delim(alzfile@filePath, check.names=FALSE)
+normdata <- read.delim(normfile@filePath, check.names=FALSE)
+metadata <- read.delim(metafile@filePath)
+
+## Get meta cols
+## I know by manual inspection that they are the first 10 cols
+alzmetacolnames <- colnames(alzdata)[1:10]
+normmetacolnames <- colnames(normdata)[1:10]
+
+# Check that they're the same
+all(alzmetacolnames == normmetacolnames)
+
+# And check that they're in the same order based on reporterid
+all(alzdata$reporterid == normdata$reporterid)
+
+## Get sample cols - the rest of the columns
+alzsamplecolnames <- colnames(alzdata)[11:ncol(alzdata)]
+normsamplecolnames <- colnames(normdata)[11:ncol(normdata)]
+
+## Update metadata with disease state
+##### After doing this, there are a number of samples not in the alz or normal files
+##### They all have has.expression == FALSE, but some have genotyped == TRUE
+metadata <- transform(metadata, DiseaseState=NA)
+metadata$DiseaseStatus[match(alzsamplecolnames, metadata$TID)] <- "Alzheimer's"
+metadata$DiseaseStatus[match(normsamplecolnames, metadata$TID)] <- "Control"
+metadata <- transform(metadata, DiseaseState=factor(DiseaseState))
+
+mergeddata <- cbind(alzdata[, alzmetacolnames],
+                    alzdata[, alzsamplecolnames],
+                    normdata[, normsamplecolnames])
+colnames(mergeddata) <- gsub("^X", "", colnames(mergeddata))
+
+study <- "HBTRC"
+center <- "MSSM"
+platform <- "Agilent44Karray"
+other <- "PFC_AgeCorrected_all"
+extension <- "tsv"
+
+## write data
+newdatafilename <- paste(paste(study, center, platform, other, sep="_"),
+                         "tsv", sep=".")
+
+write.table(mergeddata, file=newdatafilename, sep="\t", row.names=FALSE, quote=FALSE)
+
+## write metadata
+dataType <- "metaData"
+extension <- "tsv"
+
+newmetafilename <- paste(paste(study, center, dataType, sep="_"),
+                         "tsv", sep=".")
+
+write.table(metadata, file=newmetafilename, sep="\t", row.names=FALSE, quote=FALSE)
