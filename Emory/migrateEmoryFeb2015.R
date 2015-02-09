aa4a3e3d4ab4781a7776298e67d2a28a42b98123
Ben Logsdon <baelogsdon@gmail.com>
2015-02-08 17:00:16 -0800

    cleaned up clinical file for emory

--- Emory/migrateEmoryFeb2015.R
+++ Emory/migrateEmoryFeb2015.R
@@ -32,6 +32,63 @@ emoryTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'Emory%\'
 
 ####SCRATCH
 clinical <- synGet(emoryTable@values$originalSynapseId[12])
-str <- paste('mv ',clinical@filePath,' ~/')
+str <- paste('cp ',clinical@filePath,' ~/')
 system(str)
+
+#clean up clinical file
+  #write function to clean up clinical file
+    #rename columns done
+    #fix NAs done
+    #split race and sex column into two columns done
+    #fill in neuropath information done
+    #replace '/' with E in apoe done
+    #remove spaces from caseNumber and proteomicsMS done
+
+
+cleanEmoryClinical <- function(){
+  emoryClinical <- read.csv('Emory//unprocessedEmoryPreliminaryClinical.csv')
+  colnames(emoryClinical) <- c('neuropathology','caseNumber','PMI','ageOfOnset','ageAtDeath','durationYr','apoe','race_sex','proteomicsMS')
+  emoryClinical$PMI[grep("NA",emoryClinical$PMI)] <- NA
+  race_sex <- emoryClinical$race_sex;
+  race <- rep(NA,nrow(emoryClinical));
+  sex <- rep(NA,nrow(emoryClinical));
+  race[grep('a',race_sex)] <- 'Asian'
+  race[grep('b',race_sex)] <- 'AfricanAmerican'
+  race[grep('w',race_sex)] <- 'Caucasian'
+  race[grep('h',race_sex)] <- 'Hispanic'
+  sex[grep('w',race_sex)] <- 'Female'
+  sex[grep('m',race_sex)] <- 'Male'
+  emoryClinical <- data.frame(emoryClinical,race,sex)
+  emoryClinical <- emoryClinical[,-8]
+  emoryClinical$caseNumber <- gsub(' ','',emoryClinical$caseNumber)
+  emoryClinical$proteomicsMS <- gsub(' ','',emoryClinical$proteomicsMS)
+  emoryClinical$apoe <- gsub('/','E',emoryClinical$apoe)
+  emoryClinical$neuropathology[grep('AD',emoryClinical$caseNumber)] <- 'AD'
+  emoryClinical$neuropathology[grep('ADPD',emoryClinical$caseNumber)] <- 'ADPD'
+  emoryClinical$neuropathology[grep('ALS',emoryClinical$caseNumber)] <- 'ALS'
+  emoryClinical$neuropathology[grep('CBD',emoryClinical$caseNumber)] <- 'CBD'
+  emoryClinical$neuropathology[grep('CONTROL',emoryClinical$caseNumber)] <- 'Control'
+  emoryClinical$neuropathology[grep('FTDU',emoryClinical$caseNumber)] <- 'FTDU'
+  emoryClinical$neuropathology[grep('MCI',emoryClinical$caseNumber)] <- 'MCI'
+  emoryClinical$neuropathology[grep('PD',emoryClinical$caseNumber)] <- 'PD'
+  return(emoryClinical)
+}
+
+emoryClinicalClean <- cleanEmoryClinical()
+
+#move clinical to Table in staging
+  #change file name
+  #add provenance
+  #add annotations
+
+#move clinical to csv in staging
+
+
+####move raw count files
+
+
+####move refseq mapped files
+
+
+####move reference genome file
 
