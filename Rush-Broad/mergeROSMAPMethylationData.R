149eb499e9c6dbe4da476e0cd5aca68ede41e9dd
Kenneth Daily <kmdaily@gmail.com>
2015-02-09 17:26:14 -0800

    removed data file subset, added sorting by chrom, updated readData fxn

--- Rush-Broad/mergeROSMAPMethylationData.R
+++ Rush-Broad/mergeROSMAPMethylationData.R
@@ -2,6 +2,7 @@ library(synapseClient)
 library(plyr)
 library(dplyr)
 library(reshape2)
+library(rGithubClient)
 
 synapseLogin()
 
@@ -12,7 +13,7 @@ thisScript <- getPermlink(thisRepo, "Rush-Broad/mergeROSMAPMethylationData.R")
 # Function to get all files from Synapse
 readData <- function(x) {
   o <- synGet(x$id)
-  # read.delim(o@filePath)
+  read.delim(o@filePath)
 }
 
 ## Data in this folder
@@ -24,15 +25,12 @@ colnames(res) <- gsub(".*\\.", "", colnames(res))
 ## Get list of annotation files
 resAnnot <- filter(res, grepl("^ill450kAnno", name))
 
-## Get list of data files
-resData <- filter(res, grepl("^ill450kMeth", name))
-
 ## Make merged annotation
 mergedAnnot <- ddply(resAnnot, .(id), readData)
 mergedAnnot$id <- NULL
 
-## sort annotations on probe TargetID
-mergedAnnot <- arrange(mergedAnnot, TargetID)
+## sort annotations on chromosome and probe TargetID
+mergedAnnot <- arrange(mergedAnnot, CHR, TargetID)
 
 ## Write merged annotation
 study <- "ROSMAP"
