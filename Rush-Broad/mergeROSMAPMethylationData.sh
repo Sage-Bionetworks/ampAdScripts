95697c9ac9427ac84b4c964f891890f5ae1317bf
Kenneth Daily <kmdaily@gmail.com>
2015-02-10 12:18:41 -0800

    Added annotations, new filename convention

--- Rush-Broad/mergeROSMAPMethylationData.sh
+++ Rush-Broad/mergeROSMAPMethylationData.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-OUTPUTFILE=ROSMAP_Rush-Broad_IlluminaHumanMethylation450_740_imputed.tsv
+consortium="AMP-AD"
+study="ROSMAP"
+center="Rush-Broad"
+platform="IlluminaHumanMethylation450"
+other="740_imputed"
+extension="tsv"
+tissueType="Dorsolateral Prefrontal Cortex"
+tissueTypeAbrv="PFC"
+organism="human"
+dataType="Methylation"
+
+OUTPUTFILE=${consortium}_${study}_${center}_${platform}_${other}.${extension}
 LISTINGFILE=ill450kMeth_inputfiles.txt
-NEWPARENTID=syn3157275
 
 # Get the file/synapse id listing sorted by numerical chromosome
 synapse query "select id,name from file where parentId=='syn2701448'" | grep ill450kMeth | sed "s/ill450kMeth_chr//" | sort -h | cut -f 2 > ${LISTINGFILE}
@@ -17,5 +27,13 @@ cat ${LISTINGFILE} | xargs -n 1 -I{} synapse cat {} >> ${OUTPUTFILE}
 # synapse cat can't use tail, so we get extra headers. Remove them now.
 sed -i -e '2,${/^TargetID/d;}' ${OUTPUTFILE}
 
+# Compress it
+gzip ${OUTPUTFILE}
+
 # Now, store the file, add annotations and provenance
-synapse store ${OUTPUTFILE} --parentId ${NEWPARENTID} --used `cat ${LISTINGSFILE} | xargs` --annotations '{}'
+NAME="${consortium} ${study} ${center} ${platform} ${other}"
+NEWPARENTID=syn3157275
+usedIds=`cat ${LISTINGFILE} | xargs`
+annotations="'{\"consortium\": \"${consortium}\", \"platform\" : \"${platform}\", \"study\": \"${study}\", \"center\": \"${center}\", \"dataType\": \"${dataType}\", \"organism\": \"${organism}\", \"tissueType\": \"${tissueType}\", \"tissueTypeAbrv\": \"${tissueTypeAbrv}\"}'"
+
+# echo synapse store ${OUTPUTFILE}.gz --parentId ${NEWPARENTID} --name \"${NAME}\" --used ${usedIds} --annotations ${annotations}
