a7fae0f02c99e14cd37bf4e728baeeea5f9bcf01
Ben Logsdon <baelogsdon@gmail.com>
2015-02-10 14:30:15 -0500

    updated migration scripts

--- Rush-Broad/migrateROSMAPFeb2015.R
+++ Rush-Broad/migrateROSMAPFeb2015.R
@@ -4,7 +4,34 @@ synapseLogin()
 
 ##query master table for emory files
 rosmapTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'ROSMAP%\'',loadResult = TRUE)
-wclin <- grep('.xls',rosmapTable@values$oldFileName)
+fileOrganization <- unique(rosmapTable@values$data)
+
+#syn3157275 (Methylation)
+#syn3157322 (Clinical)
+#syn3157325 (genotype)
+#syn3157329 (imputed genotype)
+
+rosmapTable@values$newParentId[rosmapTable@values$data=='ROSMAP Methylation'] <- 'syn3157275'
+rosmapTable@values$toBeMigrated[rosmapTable@values$data=='ROSMAP Methylation'] <- TRUE
+rosmapTable@values$newParentId[rosmapTable@values$data=='ROSMAP Clinical'] <- 'syn3157322'
+rosmapTable@values$toBeMigrated[rosmapTable@values$data=='ROSMAP Clinical'] <- TRUE
+rosmapTable@values$newParentId[rosmapTable@values$data=='ROSMAP Genotypes'] <- 'syn3157325'
+rosmapTable@values$toBeMigrated[rosmapTable@values$data=='ROSMAP Genotypes'] <- TRUE
+
+rosmapTable@values$newParentId[rosmapTable@values$data=='ROSMAP Imputed Genotypes'] <- 'syn3157329'
+rosmapTable@values$toBeMigrated[rosmapTable@values$data=='ROSMAP Imputed Genotypes'] <- TRUE
+
+rosmapTable@values$newParentId[rosmapTable@values$data=='ROSMAP Imputed Genotypes CHOP'] <- 'syn3157329'
+rosmapTable@values$toBeMigrated[rosmapTable@values$data=='ROSMAP Imputed Genotypes CHOP'] <- FALSE
+rosmapTable@values$newSynapseId <- 'syn3157329'
+
+rosmapTable <- synStore(rosmapTable)
+
+#for (i in fileOrganization){
+#  rosmapTable@values$newParentId[rosmapTable@values$data==i] <
+#}
+
+#wclin <- grep('.xls',rosmapTable@values$oldFileName)
 
 
 #scratch
