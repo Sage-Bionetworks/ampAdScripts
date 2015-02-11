f2cb21ec90d771a0c2b21357814b5318fc49aeb6
Kenneth Daily <kmdaily@gmail.com>
2015-02-10 16:21:13 -0800

    Updated.

--- Rush-Broad/mergeROSMAPMethylationData.sh
+++ Rush-Broad/mergeROSMAPMethylationData.sh
@@ -14,21 +14,21 @@ dataType="Methylation"
 OUTPUTFILE=${consortium}_${study}_${center}_${platform}_${other}.${extension}
 LISTINGFILE=ill450kMeth_inputfiles.txt
 
-# Get the file/synapse id listing sorted by numerical chromosome
-synapse query "select id,name from file where parentId=='syn2701448'" | grep ill450kMeth | sed "s/ill450kMeth_chr//" | sort -h | cut -f 2 > ${LISTINGFILE}
+# # Get the file/synapse id listing sorted by numerical chromosome
+# synapse query "select id,name from file where parentId=='syn2701448'" | grep ill450kMeth | sed "s/ill450kMeth_chr//" | sort -h | cut -f 2 > ${LISTINGFILE}
 
-# Concatenate the data files
-cat ${LISTINGFILE} | xargs -n 1 -I{} synapse cat {} >> ${OUTPUTFILE}
+# # Concatenate the data files
+# cat ${LISTINGFILE} | xargs -n 1 -I{} synapse cat {} >> ${OUTPUTFILE}
 
-# # synapse cat needs fixing because it prints extra newlines
-# # Using branch fixsynapsecat until it gets merged into devel, but this would work too
-# sed -i -e "/^$/d" ${OUTPUTFILE}
+# # # synapse cat needs fixing because it prints extra newlines
+# # # Using branch fixsynapsecat until it gets merged into devel, but this would work too
+# # sed -i -e "/^$/d" ${OUTPUTFILE}
 
-# synapse cat can't use tail, so we get extra headers. Remove them now.
-sed -i -e '2,${/^TargetID/d;}' ${OUTPUTFILE}
+# # synapse cat can't use tail, so we get extra headers. Remove them now.
+# sed -i -e '2,${/^TargetID/d;}' ${OUTPUTFILE}
 
-# Compress it
-gzip ${OUTPUTFILE}
+# # Compress it
+# gzip ${OUTPUTFILE}
 
 # Now, store the file, add annotations and provenance
 NAME="${consortium} ${study} ${center} ${platform} ${other}"
@@ -36,4 +36,11 @@ NEWPARENTID=syn3157275
 usedIds=`cat ${LISTINGFILE} | xargs`
 annotations="'{\"consortium\": \"${consortium}\", \"platform\" : \"${platform}\", \"study\": \"${study}\", \"center\": \"${center}\", \"dataType\": \"${dataType}\", \"organism\": \"${organism}\", \"tissueType\": \"${tissueType}\", \"tissueTypeAbrv\": \"${tissueTypeAbrv}\"}'"
 
-synapse store ${OUTPUTFILE}.gz --parentId ${NEWPARENTID} --name \"${NAME}\" --used ${usedIds} --annotations ${annotations}
+# Current path to commit
+# Not really correct b/c I can't commit and then change...
+thisScript="https://raw.githubusercontent.com/Sage-Bionetworks/ampAdScripts/99f780d4d97c2b0d60fc1acca9f9c5a9a4b8f0d2/Rush-Broad/mergeROSMAPMethylationData.sh"
+
+# the synapse command does not work properly when run inside the script
+# it may need to be run manually
+echo synapse --debug store ${OUTPUTFILE}.gz --parentId ${NEWPARENTID} --name \"${NAME}\" --used ${usedIds} --executed ${thisScript} --annotations ${annotations}
+synapse --debug store ${OUTPUTFILE}.gz --parentId ${NEWPARENTID} --name \"${NAME}\" --used ${usedIds} --executed \"${thisScript}\" --annotations ${annotations}
