d45977463aeccce308f49e4fa96d09c96461d03f
Kenneth Daily <kmdaily@gmail.com>
2015-02-11 12:08:06 -0800

    Updated.

--- Rush-Broad/mergeROSMAPMethylationData.sh
+++ Rush-Broad/mergeROSMAPMethylationData.sh
@@ -12,21 +12,21 @@ dataType="Methylation"
 OUTPUTFILE=${consortium}_${study}_${center}_${platform}_${other}.${extension}
 LISTINGFILE=ill450kMeth_inputfiles.txt
 
-# # Get the file/synapse id listing sorted by numerical chromosome
-# synapse query "select id,name from file where parentId=='syn2701448'" | grep ill450kMeth | sed "s/ill450kMeth_chr//" | sort -h | cut -f 2 > ${LISTINGFILE}
+# Get the file/synapse id listing sorted by numerical chromosome
+synapse query "select id,name from file where parentId=='syn2701448'" | grep ill450kMeth | sed "s/ill450kMeth_chr//" | sort -h | cut -f 2 > ${LISTINGFILE}
 
-# # Concatenate the data files
-# cat ${LISTINGFILE} | xargs -n 1 -I{} synapse cat {} >> ${OUTPUTFILE}
+# Concatenate the data files
+cat ${LISTINGFILE} | xargs -n 1 -I{} synapse cat {} >> ${OUTPUTFILE}
 
-# # # synapse cat needs fixing because it prints extra newlines
-# # # Using branch fixsynapsecat until it gets merged into devel, but this would work too
-# # sed -i -e "/^$/d" ${OUTPUTFILE}
+# # synapse cat needs fixing because it prints extra newlines
+# # Using branch fixsynapsecat until it gets merged into devel, but this would work too
+# sed -i -e "/^$/d" ${OUTPUTFILE}
 
-# # synapse cat can't use tail, so we get extra headers. Remove them now.
-# sed -i -e '2,${/^TargetID/d;}' ${OUTPUTFILE}
+# synapse cat can't use tail, so we get extra headers. Remove them now.
+sed -i -e '2,${/^TargetID/d;}' ${OUTPUTFILE}
 
-# # Compress it
-# gzip ${OUTPUTFILE}
+# Compress it
+gzip ${OUTPUTFILE}
 
 # Now, store the file, add annotations and provenance
 NAME="${consortium}_${study}_${center}_${platform}_${other}"
