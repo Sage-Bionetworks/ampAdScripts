c3502d1d6448c310f17ad33947428e6976f697da
Ben Logsdon <blogsdon@DHCP149223.FHCRC.ORG>
2015-02-12 11:10:19 -0800

    updated imputed genotype migration script

--- Rush-Broad/migrateROSMAPImputedGenotypesFeb2015.R
+++ Rush-Broad/migrateROSMAPImputedGenotypesFeb2015.R
@@ -1,57 +1,128 @@
 require(synapseClient)
 synapseLogin()
 
-##query master table for emory files
-#rosmapTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'ROSMAP%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
-
-
-
 moveGeno <- function(i,a,newFileName,rosmapTable){
-  fileType <- strsplit(rosmapTable@values$oldFileName[i],'\\.')[[1]][4]
-  newFileName <- paste0(newFileName,'.',fileType)
-  cat(newFileName,'\n')
-  system(paste('cp ',a[[i]]@filePath,' ',newFileName,sep=''))
+  foo <- strsplit(rosmapTable@values$oldFileName[i],'\\.')[[1]]
+  fileType <- foo[length(foo)]
+  if (fileType=='gz'){
+    newFileName <- paste0(newFileName,'_',foo[1],'_Group',foo[3],'.',fileType)
+    cat(newFileName,'\n')
+    system(paste('cp ',a[[i]]@filePath,' ',newFileName,sep=''))    
+  } else if (fileType=='fam'){
+    newFileName <- paste0(newFileName,'_Group',strsplit(foo[[1]],'list')[[1]][2],'.',fileType)
+    cat(newFileName,'\n')
+    system(paste('cp ',a[[i]]@filePath,' ',newFileName,sep=''))    
+  } else if (fileType=='backup'){
+    newFileName <- paste0(newFileName,'.fam')
+    cat(newFileName,'\n')
+    system(paste('cp ',a[[i]]@filePath,' ',newFileName,sep=''))    
+  }
 }
 
 makeFile <- function(i,a,newEntityName,newFileName,rosmapTable){
-  rosmapTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'ROSMAP Genotypes%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
-  fileType <- strsplit(rosmapTable@values$oldFileName[i],'\\.')[[1]][4]
-  newFileName <- paste0(newFileName,'.',fileType)
-  b <- File(newFileName,parentId=rosmapTable@values$newParentId[i],name=paste0(newEntityName,'_',fileType))
-  dataAnnotation <- list(
-    dataType = 'DNA',
-    disease = c('Alzheimers Disease','Control'),
-    platform = 'Affymetrix Genechip 6.0',
-    center = 'Rush-Broad',
-    study = 'ROSMAP',
-    fileType = 'plink',
-    organism = 'human'
-  )
-  synSetAnnotations(b) <- dataAnnotation
-  act <- Activity(name='ROSMAP Genotype Migration',
-                  used=list(list(entity=rosmapTable@values$originalSynapseId[i],wasExecuted=F)),
-                  executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/Rush-Broad/migrateROSMAPGenotypesFeb2015.R"))
-  act <- storeEntity(act)
-  generatedBy(b) <- act
-  b <- synStore(b)
-  rosmapTable@values$newSynapseId[i] <- b$properties$id
-  wind <- is.na(rosmapTable@values$newSynapseId)
-  if(sum(wind)>0){
-    rosmapTable@values$newSynapseId[wind] <- ''
+  rosmapTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'ROSMAP Imputed Genotypes%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
+  foo <- strsplit(rosmapTable@values$oldFileName[i],'\\.')[[1]]
+  fileType <- foo[length(foo)]
+  if(fileType == 'gz'){
+    newFileName <- paste0(newFileName,'_',foo[1],'_Group',foo[3],'.',fileType)
+    b <- File(newFileName,parentId=rosmapTable@values$newParentId[i],name=paste0(newFileName,'_',foo[1],'_Group',foo[3],'_',fileType))
+    dataAnnotation <- list(
+      dataType = 'DNA',
+      disease = c('Alzheimers Disease','Control'),
+      platform = 'Affymetrix Genechip 6.0',
+      center = 'Rush-Broad',
+      study = 'ROSMAP',
+      fileType = 'plink',
+      organism = 'human'
+    )
+    synSetAnnotations(b) <- dataAnnotation
+    act <- Activity(name='ROSMAP Genotype Migration',
+                    used=list(list(entity=rosmapTable@values$originalSynapseId[i],wasExecuted=F)),
+                    executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/Rush-Broad/migrateROSMAPGenotypesFeb2015.R"))
+    act <- storeEntity(act)
+    generatedBy(b) <- act
+    b <- synStore(b)
+    rosmapTable@values$newSynapseId[i] <- b$properties$id
+    wind <- is.na(rosmapTable@values$newSynapseId)
+    if(sum(wind)>0){
+      rosmapTable@values$newSynapseId[wind] <- ''
+    }
+    rosmapTable@values$newFileName[i] <- newFileName
+    rosmapTable@values$isMigrated[i] <- TRUE
+    rosmapTable@values$hasAnnotation[i] <- TRUE
+    rosmapTable@values$hasProvenance[i] <- TRUE
+    rosmapTable <- synStore(rosmapTable)   
+    system(paste0('rm ',newFileName))
+  }else if (fileType=='backup'){
+    newFileName <- paste0(newFileName,'.fam')
+    b <- File(newFileName,parentId=rosmapTable@values$newParentId[i],name=paste0(newEntityName,'_fam'))
+    dataAnnotation <- list(
+      dataType = 'DNA',
+      disease = c('Alzheimers Disease','Control'),
+      platform = 'Affymetrix Genechip 6.0',
+      center = 'Rush-Broad',
+      study = 'ROSMAP',
+      fileType = 'plink',
+      organism = 'human'
+    )
+    synSetAnnotations(b) <- dataAnnotation
+    act <- Activity(name='ROSMAP Genotype Migration',
+                    used=list(list(entity=rosmapTable@values$originalSynapseId[i],wasExecuted=F)),
+                    executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/Rush-Broad/migrateROSMAPGenotypesFeb2015.R"))
+    act <- storeEntity(act)
+    generatedBy(b) <- act
+    b <- synStore(b)
+    rosmapTable@values$newSynapseId[i] <- b$properties$id
+    wind <- is.na(rosmapTable@values$newSynapseId)
+    if(sum(wind)>0){
+      rosmapTable@values$newSynapseId[wind] <- ''
+    }
+    rosmapTable@values$newFileName[i] <- newFileName
+    rosmapTable@values$isMigrated[i] <- TRUE
+    rosmapTable@values$hasAnnotation[i] <- TRUE
+    rosmapTable@values$hasProvenance[i] <- TRUE
+    rosmapTable <- synStore(rosmapTable)    
+    system(paste0('rm ',newFileName))
+  }else if (fileType=='fam'){
+    newFileName <- paste0(newFileName,'_Group',strsplit(foo[[1]],'list')[[1]][2],'.',fileType)
+    b <- File(newFileName,parentId=rosmapTable@values$newParentId[i],name=paste0(newEntityName,'_Group',strsplit(foo[[1]],'list')[[1]][2],'_',fileType))
+    dataAnnotation <- list(
+      dataType = 'DNA',
+      disease = c('Alzheimers Disease','Control'),
+      platform = 'Affymetrix Genechip 6.0',
+      center = 'Rush-Broad',
+      study = 'ROSMAP',
+      fileType = 'plink',
+      organism = 'human'
+    )
+    synSetAnnotations(b) <- dataAnnotation
+    act <- Activity(name='ROSMAP Genotype Migration',
+                    used=list(list(entity=rosmapTable@values$originalSynapseId[i],wasExecuted=F)),
+                    executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/Rush-Broad/migrateROSMAPGenotypesFeb2015.R"))
+    act <- storeEntity(act)
+    generatedBy(b) <- act
+    b <- synStore(b)
+    rosmapTable@values$newSynapseId[i] <- b$properties$id
+    wind <- is.na(rosmapTable@values$newSynapseId)
+    if(sum(wind)>0){
+      rosmapTable@values$newSynapseId[wind] <- ''
+    }
+    rosmapTable@values$newFileName[i] <- newFileName
+    rosmapTable@values$isMigrated[i] <- TRUE
+    rosmapTable@values$hasAnnotation[i] <- TRUE
+    rosmapTable@values$hasProvenance[i] <- TRUE
+    rosmapTable <- synStore(rosmapTable)
+    system(paste0('rm ',newFileName))
   }
-  rosmapTable@values$newFileName[i] <- newFileName
-  rosmapTable@values$isMigrated[i] <- TRUE
-  rosmapTable@values$hasAnnotation[i] <- TRUE
-  rosmapTable@values$hasProvenance[i] <- TRUE
-  rosmapTable <- synStore(rosmapTable) 
+
 }
 
-migrateGenotype <- function(){
-  rosmapTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'ROSMAP Genotypes%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
+migrateImputedGenotype <- function(){
+  rosmapTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'ROSMAP Imputed Genotypes%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
   synList <- sapply(rosmapTable@values$originalSynapseId,synGet)
-  newFileName <- 'AMP-AD_ROSMAP_Rush-Broad_AffymetrixGenechip6'
-  newEntityName <- 'ROSMAP_Rush-Broad_AffymetrixGenechip6'
+  newFileName <- 'AMP-AD_ROSMAP_Rush-Broad_AffymetrixGenechip6_Imputed'
+  newEntityName <- 'ROSMAP_Rush-Broad_AffymetrixGenechip6_Imputed'
   sapply(1:nrow(rosmapTable@values),moveGeno,synList,newFileName,rosmapTable)
   sapply(1:nrow(rosmapTable@values),makeFile,synList,newEntityName,newFileName,rosmapTable)
 }
-migrateGenotype()
+migrateImputedGenotype()
