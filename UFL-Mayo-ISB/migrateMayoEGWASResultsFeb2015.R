d8cceb64b375beeec6c5b8626e3d234f3b5eae92
Ben Logsdon <baelogsdon@gmail.com>
2015-02-14 19:57:58 -0800

    updated egwas results migration script

--- UFL-Mayo-ISB/migrateMayoEGWASResultsFeb2015.R
+++ UFL-Mayo-ISB/migrateMayoEGWASResultsFeb2015.R
@@ -4,33 +4,51 @@ synapseLogin()
 ##query master table for emory files
 #mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'mayo%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
 
-
-
 moveGeno <- function(i,a,newFileName,mayoTable){
-  fileType <- strsplit(mayoTable@values$oldFileName[i],'\\.')[[1]][2]
-  newFileName <- paste0(newFileName,'.',fileType)
+  fileType <- strsplit(mayoTable@values$oldFileName[i],'\\.')[[1]][1]
+  newFileName <- paste0(newFileName,'_',fileType,'.gz')
   cat(newFileName,'\n')
   system(paste('cp ',a[[i]]@filePath,' ',newFileName,sep=''))
 }
 
 makeFile <- function(i,a,newEntityName,newFileName,mayoTable){
   mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'MayoCC Genotype%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
-  fileType <- strsplit(mayoTable@values$oldFileName[i],'\\.')[[1]][2]
-  newFileName <- paste0(newFileName,'.',fileType)
+  fileType <- strsplit(mayoTable@values$oldFileName[i],'\\.')[[1]][1]
+  newFileName <- paste0(newFileName,'_',fileType,'.gz')
   b <- File(newFileName,parentId=mayoTable@values$newParentId[i],name=paste0(newEntityName,'_',fileType))
+  foo <- strsplit(mayoTable@values$oldFileName[i],'_')[[1]]
+  if(foo[1]=='HapMap2'){
+    dataSubType <- 'imputedGenotypes'
+  } else {
+    dataSubType <- 'Genotypes'
+  }
+  if (foo[2]=='CER'){
+    tissueType <- 'Cerebellum'
+  } else{
+    tissueType <- 'Temporal Cortex'
+  }
+  if (foo[3]=='AD.txt.gz'){
+    disease <- 'Alzheimers Disease'
+  } else if (foo[3]=='Control.txt.gz'){
+    disease <- 'Control'
+  } else {
+    disease <- c('Alzheimers Disease', 'Control')
+  }
+  
   dataAnnotation <- list(
-    dataType = 'DNA',
-    disease = c('Alzheimers Disease','Control','Progressive Supranuclear Palsy'),
-    platform = 'TaqMan',
+    dataType = 'Analysis',
+    disease = disease,
+    dataSubType = dataSubType,
+    tissueType = tissueType,
     center = 'UFL-Mayo-ISB',
-    study = 'MayoLOADGWAS',
-    fileType = 'plink',
+    study = 'MayoEGWAS',
+    fileType = 'tsv',
     organism = 'human'
   )
   synSetAnnotations(b) <- dataAnnotation
-  act <- Activity(name='Mayo TLR Genotype Migration',
+  act <- Activity(name='Mayo EGWAS Results Migration',
                   used=list(list(entity=mayoTable@values$originalSynapseId[i],wasExecuted=F)),
-                  executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/UFL-Mayo-ISB/migrateMayoTLRGenotypesFeb2015.R"))
+                  executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/UFL-Mayo-ISB/migrateMayoEGWASResultsFeb2015.R"))
   act <- storeEntity(act)
   generatedBy(b) <- act
   b <- synStore(b)
@@ -47,15 +65,15 @@ makeFile <- function(i,a,newEntityName,newFileName,mayoTable){
   system(paste0('rm ',newFileName))
 }
 
-migrateGenotype <- function(){
-  mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'MayoCC Genotype%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
+migrateEGWASResults <- function(){
+  mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'MayoCC Expression GWAS%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
   synList <- sapply(mayoTable@values$originalSynapseId,synGet)
-  newFileName <- 'AMP-AD_MayoLOADGWAS_UFL-Mayo-ISB_TaqMan'
-  newEntityName <- 'MayoLOADGWAS_UFL-Mayo-ISB_TaqMan'
-  sapply(5:7,moveGeno,synList,newFileName,mayoTable)
-  sapply(5:7,makeFile,synList,newEntityName,newFileName,mayoTable)
+  newFileName <- 'AMP-AD_MayoEGWAS_UFL-Mayo-ISB_Results'
+  newEntityName <- 'MayoEGWAS_UFL-Mayo-ISB_Results'
+  sapply(1:12,moveGeno,synList,newFileName,mayoTable)
+  sapply(1:12,makeFile,synList,newEntityName,newFileName,mayoTable)
 }
-migrateGenotype()
+migrateEGWASResults()
 
 
 
