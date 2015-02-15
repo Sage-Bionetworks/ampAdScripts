f0e2513ffbb0690a41577c0dd35afeeb5f9bbfdb
Ben Logsdon <baelogsdon@gmail.com>
2015-02-14 19:59:11 -0800

    fixed bug in script

--- UFL-Mayo-ISB/migrateMayoEGWASResultsFeb2015.R
+++ UFL-Mayo-ISB/migrateMayoEGWASResultsFeb2015.R
@@ -12,7 +12,7 @@ moveGeno <- function(i,a,newFileName,mayoTable){
 }
 
 makeFile <- function(i,a,newEntityName,newFileName,mayoTable){
-  mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'MayoCC Genotype%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
+  mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'MayoCC Expression GWAS%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
   fileType <- strsplit(mayoTable@values$oldFileName[i],'\\.')[[1]][1]
   newFileName <- paste0(newFileName,'_',fileType,'.gz')
   b <- File(newFileName,parentId=mayoTable@values$newParentId[i],name=paste0(newEntityName,'_',fileType))
