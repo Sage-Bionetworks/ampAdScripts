03a93bea6cc6b0e46e0390c25d5680316cef4184
Ben Logsdon <blogsdon@DHCP149223.FHCRC.ORG>
2015-02-16 08:59:48 -0800

    updated version of tau mouse data

--- UFL-Mayo-ISB/migrateUFLTauFeb2015.R
+++ UFL-Mayo-ISB/migrateUFLTauFeb2015.R
@@ -1,119 +1,75 @@
-#Cer 'clinical' syn3162960
-#TCX 'clinical' syn3162961
-#Cer 'technical syn3161029
-#TCX 'technical syn3161032
-
 require(synapseClient)
 synapseLogin()
+migrateTau <- function(i,newFileName,newEntityName,other){
+  mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'UFL%RNAseq%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
+  normalized <- synGet(mayoTable@values$originalSynapseId[i])
+  system(paste('cp ',normalized@filePath,' ',newFileName,sep=''))
 
-mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'UFL%Nanostring%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
+  b <- File(newFileName,parentId=mayoTable@values$newParentId[i],name=newEntityName)
+  dataAnnotation <- list(
+    dataType = 'mRNA',
+    disease = c('Alzheimers'),
+    platform= 'IlluminaHiSeq2000',
+    mouseModel = 'Tau',
+    center = 'UFL-Mayo-ISB',
+    study = 'UFL',
+    fileType = 'tsv',
+    organism = 'mouse',
+    other = other
+  )
+  synSetAnnotations(b) <- dataAnnotation
+  act <- Activity(name='UFL Tau RNAseq Data Migration',
+                  used=list(list(entity=mayoTable@values$originalSynapseId[i],wasExecuted=F)),
+                  executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/UFL-Mayo-ISB/migrateUFLTauFeb2015.R"))
+  act <- storeEntity(act)
+  generatedBy(b) <- act
+  b <- synStore(b)
+  mayoTable@values$newSynapseId[i] <- b$properties$id
+  wind <- is.na(mayoTable@values$newSynapseId)
+  if(sum(wind)>0){
+    mayoTable@values$newSynapseId[wind] <- ''
+  }
+  mayoTable@values$newFileName[i] <- newFileName
+  mayoTable@values$isMigrated[i] <- TRUE
+  mayoTable@values$hasAnnotation[i] <- TRUE
+  mayoTable@values$hasProvenance[i] <- TRUE
+  mayoTable <- synStore(mayoTable)
+  system(paste0('rm ',newFileName))
+}
 
+migrateTau(2,'AMP-AD_UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_GeneCounts.txt.gz','UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_GeneCounts',other=c('geneCounts','Unnormalized'))
+migrateTau(3,'AMP-AD_UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_TranscriptCounts.txt.gz','UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_TranscriptCounts',other=c('transcriptCounts','Unnormalized'))
+migrateTau(4,'AMP-AD_UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_GeneCounts_Normalized.txt.gz','UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_GeneCounts_Normalized',other=c('geneCounts','Normalized'))
+migrateTau(5,'AMP-AD_UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_GeneCounts_Transposed.txt.gz','UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_GeneCounts_Transposed',other=c('geneCounts','Unnormalized','Transposed'))
+migrateTau(6,'AMP-AD_UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_TranscriptCounts_Normalized.txt.gz','UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_TranscriptCounts_Normalized',other=c('geneCounts','Normalized'))
+migrateTau(7,'AMP-AD_UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_TranscriptCounts_Transposed.txt.gz','UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_TranscriptCounts_Transposed',other=c('geneCounts','Unnormalized','Transposed'))
+
+
+#covariates
+
+mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'UFL%RNAseq%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
 i <- 1
 normalized <- synGet(mayoTable@values$originalSynapseId[i])
 normalizedData <- read.table(normalized@filePath,header=T)
 
-newFileName <- 'AMP-AD_UFL_UFL-Mayo-ISB_Nanostring_APP_IL10_Normalized.csv'
-newEntityName <- 'UFL_UFL-Mayo-ISB_Nanostring_APP_IL10_Normalized'
-write.csv(normalizedData,file=newFileName,row.names=F)
-
-b <- File(newFileName,parentId=mayoTable@values$newParentId[i],name=newEntityName)
-dataAnnotation <- list(
-  dataType = 'mRNA',
-  disease = c('Alzheimers'),
-  mouseModel = 'APP',
-  center = 'UFL-Mayo-ISB',
-  study = 'UFL',
-  fileType = 'csv',
-  organism = 'mouse',
-  other = 'Normalized'
-)
-synSetAnnotations(b) <- dataAnnotation
-act <- Activity(name='UFL Nanostring Data Migration',
-                used=list(list(entity=mayoTable@values$originalSynapseId[i],wasExecuted=F)),
-                executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/UFL-Mayo-ISB/migrateUFLnanostringFeb2015.R"))
-act <- storeEntity(act)
-generatedBy(b) <- act
-b <- synStore(b)
-mayoTable@values$newSynapseId[i] <- b$properties$id
-wind <- is.na(mayoTable@values$newSynapseId)
-if(sum(wind)>0){
-  mayoTable@values$newSynapseId[wind] <- ''
-}
-mayoTable@values$newFileName[i] <- newFileName
-mayoTable@values$isMigrated[i] <- TRUE
-mayoTable@values$hasAnnotation[i] <- TRUE
-mayoTable@values$hasProvenance[i] <- TRUE
-mayoTable <- synStore(mayoTable)
-system(paste0('rm ',newFileName))
-
-
-#raw data
-mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'UFL%Nanostring%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
-
-i <- 2
-normalized <- synGet(mayoTable@values$originalSynapseId[i])
-normalizedData <- read.table(normalized@filePath,header=T)
-
-newFileName <- 'AMP-AD_UFL_UFL-Mayo-ISB_Nanostring_APP_IL10_Unnormalized.csv'
-newEntityName <- 'UFL_UFL-Mayo-ISB_Nanostring_APP_IL10_Unnormalized'
-write.csv(normalizedData,file=newFileName,row.names=F)
-
-b <- File(newFileName,parentId=mayoTable@values$newParentId[i],name=newEntityName)
-dataAnnotation <- list(
-  dataType = 'mRNA',
-  disease = c('Alzheimers'),
-  mouseModel = 'APP',
-  center = 'UFL-Mayo-ISB',
-  study = 'UFL',
-  fileType = 'csv',
-  organism = 'mouse',
-  other = 'Unnormalized'
-)
-synSetAnnotations(b) <- dataAnnotation
-act <- Activity(name='UFL Nanostring Data Migration',
-                used=list(list(entity=mayoTable@values$originalSynapseId[i],wasExecuted=F)),
-                executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/UFL-Mayo-ISB/migrateUFLnanostringFeb2015.R"))
-act <- storeEntity(act)
-generatedBy(b) <- act
-b <- synStore(b)
-mayoTable@values$newSynapseId[i] <- b$properties$id
-wind <- is.na(mayoTable@values$newSynapseId)
-if(sum(wind)>0){
-  mayoTable@values$newSynapseId[wind] <- ''
-}
-mayoTable@values$newFileName[i] <- newFileName
-mayoTable@values$isMigrated[i] <- TRUE
-mayoTable@values$hasAnnotation[i] <- TRUE
-mayoTable@values$hasProvenance[i] <- TRUE
-mayoTable <- synStore(mayoTable)
-system(paste0('rm ',newFileName))
-
-##covariates
-
-mayoTable <- synTableQuery('SELECT * FROM syn3163713 where data like \'UFL%Nanostring%\' and migrator=\'Ben\' and toBeMigrated=TRUE',loadResult = TRUE)
-
-i <- 3
-normalized <- synGet(mayoTable@values$originalSynapseId[i])
-normalizedData <- read.table(normalized@filePath,header=T)
-
-newFileName <- 'AMP-AD_UFL_UFL-Mayo-ISB_Nanostring_APP_IL10_Covariates.csv'
-newEntityName <- 'UFL_UFL-Mayo-ISB_Nanostring_APP_IL10_Covariates'
+newFileName <- 'AMP-AD_UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_Covariates.csv'
+newEntityName <- 'UFL_UFL-Mayo-ISB_IlluminaHiSeq2000_Tau_Covariates'
 write.csv(normalizedData,file=newFileName,row.names=F)
 
 b <- File(newFileName,parentId=mayoTable@values$newParentId[i],name=newEntityName)
 dataAnnotation <- list(
   dataType = 'metaData',
   disease = c('Alzheimers'),
-  mouseModel = 'APP',
+  mouseModel = 'Tau',
   center = 'UFL-Mayo-ISB',
   study = 'UFL',
   fileType = 'csv',
   organism = 'mouse'
 )
 synSetAnnotations(b) <- dataAnnotation
-act <- Activity(name='UFL Nanostring Data Migration',
+act <- Activity(name='UFL Tau RNAseq Data Migration',
                 used=list(list(entity=mayoTable@values$originalSynapseId[i],wasExecuted=F)),
-                executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/UFL-Mayo-ISB/migrateUFLnanostringFeb2015.R"))
+                executed=list("https://github.com/Sage-Bionetworks/ampAdScripts/blob/master/UFL-Mayo-ISB/migrateUFLTauFeb2015.R"))
 act <- storeEntity(act)
 generatedBy(b) <- act
 b <- synStore(b)
@@ -128,3 +84,6 @@ mayoTable@values$hasAnnotation[i] <- TRUE
 mayoTable@values$hasProvenance[i] <- TRUE
 mayoTable <- synStore(mayoTable)
 system(paste0('rm ',newFileName))
+
+
+
