7a1708ab7739d0d8393d3de5f29d93984a1b1b59
Ben Logsdon <aeberhart@gmail.com>
2015-03-02 07:49:24 -0800

    grab all the Mayo data

--- UFL-Mayo-ISB/grabMayoEGWASdata.sh
+++ UFL-Mayo-ISB/grabMayoEGWASdata.sh
@@ -15,4 +15,13 @@ echo "Downloading Mayo LOAD GWAS genotypes bim file"
 synapse get syn3205814
 echo "Downloading Mayo LOAD GWAS genotypes fam file"
 synapse get syn3205816
-
+echo "Downloading Mayo LOAD GWAS covariates file"
+synapse get syn3205821
+echo "Downloading Mayo EGWAS Cerebellum expression data"
+synapse get syn3256501
+echo "Downloading Mayo EGWAS Cerebellum expression data covariates"
+synapse get syn3256502
+echo "Downloading Mayo EGWAS Temporal Cortex expression data"
+synapse get syn3256507
+echo "Downloading Mayo EGWAS Temporal Cortex expression data covariates"
+synapse get syn3256508
