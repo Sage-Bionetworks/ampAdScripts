a05f697fde4b752cb5de4066ce4a06cec76f60e8
Ben Logsdon <aeberhart@gmail.com>
2015-03-02 08:50:59 -0800

    updated eqtl script

---
+++ UFL-Mayo-ISB/grabMayoEGWASdata.R
@@ -0,0 +1,39 @@
+require(synapseClient)
+synapseLogin()
+
+cat("Making Directory for Analyses: MayoEGWASanalyses/\n")
+dir.create('MayoEGWASanalyses/')
+cat("Changing to Directory MayoEGWASanalyses/\n")
+setwd('MayoEGWASanalyses')
+
+cat("Downloading Mayo LOAD GWAS genotypes bed file\n")
+gwas_bed<-synGet('syn3205812',downloadLocation = './')
+cat("Downloading Mayo LOAD GWAS genotypes bim file\n")
+gwas_bim <- synGet('syn3205814',downloadLocation = './')
+cat("Downloading Mayo LOAD GWAS genotypes fam file\n")
+gwas_fam <- synGet('syn3205816',downloadLocation= './')
+cat("Downloading Mayo LOAD GWAS covariates file\n")
+gwas_cov <- synGet('syn3205821',downloadLocation='./')
+cat("Downloading Mayo EGWAS Cerebellum expression data\n")
+egwas_cere <- synGet('syn3256501',downloadLocation='./')
+cat("Downloading Mayo EGWAS Cerebellum expression data covariates\n")
+egwas_cere_cov<-synGet('syn3256502',downloadLocation='./')
+cat("Downloading Mayo EGWAS Temporal Cortex expression data\n")
+egwas_tcx <- synGet('syn3256507',downloadLocation='./')
+cat("Downloading Mayo EGWAS Temporal Cortex expression data covariates\n")
+egwas_tcx_cov <- synGet('syn3256508',downloadLocation='./')
+
+#read in covariate files
+cere_cov <- read.csv(egwas_cere_cov@filePath,header=T)
+tcx_cov <- read.csv(egwas_tcx_cov@filePath,header=T)
+
+#write ids
+write.table(cere_cov[,1:2],file='cerebellumIds.txt',sep=' ',quote=F,row.names=F,col.names=F)
+write.table(tcx_cov[,1:2],file='temporalcortexIds.txt',sep=' ',quote=F,row.names=F,col.names=F)
+
+#run plink command to filter genotypes based on shared ids
+system('./')
+
+#read in genotypes into R
+
+#
--- UFL-Mayo-ISB/grabMayoEGWASdata.sh
+++ UFL-Mayo-ISB/grabMayoEGWASdata.sh
@@ -3,25 +3,4 @@
 #Requirements:
 #synapse command line client (link)
 #plink (link)
-
-echo "Making Directory for Analyses: MayoEGWASanalyses/"
-mkdir MayoEGWASanalyses
-echo "Changing to Directory MayoEGWASanalyses/"
-cd MayoEGWASanalyses
-
-echo "Downloading Mayo LOAD GWAS genotypes bed file"
-synapse get syn3205812
-echo "Downloading Mayo LOAD GWAS genotypes bim file"
-synapse get syn3205814
-echo "Downloading Mayo LOAD GWAS genotypes fam file"
-synapse get syn3205816
-echo "Downloading Mayo LOAD GWAS covariates file"
-synapse get syn3205821
-echo "Downloading Mayo EGWAS Cerebellum expression data"
-synapse get syn3256501
-echo "Downloading Mayo EGWAS Cerebellum expression data covariates"
-synapse get syn3256502
-echo "Downloading Mayo EGWAS Temporal Cortex expression data"
-synapse get syn3256507
-echo "Downloading Mayo EGWAS Temporal Cortex expression data covariates"
-synapse get syn3256508
+plink --bfile AMP-AD_MayoLOADGWAS_UFL-Mayo-ISB_IlluminaHumanHap300 --keep cerebellumIds.txt --make-bed --out AMP-AD_MayoLOADGWAS_UFL-Mayo-ISB_IlluminaHumanHap300_Cerebellum
