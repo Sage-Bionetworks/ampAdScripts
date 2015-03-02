a6dd2df9c31d7aaa396c76b9137b7e16c247417c
Ben Logsdon <aeberhart@gmail.com>
2015-03-02 13:48:42 -0800

    eQTL script updates

--- UFL-Mayo-ISB/grabMayoEGWASdata.R
+++ UFL-Mayo-ISB/grabMayoEGWASdata.R
@@ -13,7 +13,7 @@ gwas_bim <- synGet('syn3205814',downloadLocation = './')
 cat("Downloading Mayo LOAD GWAS genotypes fam file\n")
 gwas_fam <- synGet('syn3205816',downloadLocation= './')
 cat("Downloading Mayo LOAD GWAS covariates file\n")
-gwas_cov <- synGet('syn3205821',downloadLocation='./')
+gwas_cov_syn <- synGet('syn3205821',downloadLocation='./')
 cat("Downloading Mayo EGWAS Cerebellum expression data\n")
 egwas_cere <- synGet('syn3256501',downloadLocation='./')
 cat("Downloading Mayo EGWAS Cerebellum expression data covariates\n")
@@ -24,15 +24,49 @@ cat("Downloading Mayo EGWAS Temporal Cortex expression data covariates\n")
 egwas_tcx_cov <- synGet('syn3256508',downloadLocation='./')
 
 #read in covariate files
-cere_cov <- read.csv(egwas_cere_cov@filePath,header=T)
-tcx_cov <- read.csv(egwas_tcx_cov@filePath,header=T)
-gwas_cov <- read.csv(gwas_cov@filePath,header=T)
+cere_cov <- read.csv(egwas_cere_cov@filePath,header=T,stringsAsFactors=F)
+tcx_cov <- read.csv(egwas_tcx_cov@filePath,header=T,stringsAsFactors=F)
+gwas_cov <- read.csv(gwas_cov_syn@filePath,header=T,stringsAsFactors=F)
 cere_keep <- cere_cov$IID%in%gwas_cov$IID
 tcx_keep <- tcx_cov$IID%in%gwas_cov$IID
 
+#read in expression data for cerebellum
+mayo_egwas_cere_data <- read.csv(egwas_cere@filePath,header=T)
+mayo_egwas_cere_data <- mayo_egwas_cere_data[cere_keep,]
+
+mayo_egwas_tcx_data <- read.csv(egwas_tcx@filePath,header=T)
+mayo_egwas_tcx_data <- mayo_egwas_tcx_data[tcx_keep,]
+
+cere_cov <- cere_cov[cere_keep,]
+tcx_cov <- tcx_cov[tcx_keep,]
+
+ind1 <- 1:nrow(gwas_cov);
+names(ind1) <- as.character(gwas_cov$IID)
+
+ind2 <- 1:nrow(cere_cov)
+names(ind2) <- as.character(cere_cov$IID)
+
+ind3 <- 1:nrow(tcx_cov)
+names(ind3) <- as.character(tcx_cov$IID)
+
+cereSamples <- intersect(as.character(cere_cov$IID),as.character(gwas_cov$IID))
+tcxSamples <- intersect(as.character(tcx_cov$IID),as.character(gwas_cov$IID))
+
+gwas_tcx_cov <- gwas_cov[ind1[tcxSamples],]
+gwas_cere_cov <- gwas_cov[ind1[cereSamples],]
+
+cere_cov <- cere_cov[ind2[cereSamples],]
+tcx_cov <- tcx_cov[ind3[tcxSamples],]
+
+mayo_egwas_cere_data <- mayo_egwas_cere_data[ind2[cereSamples],]
+mayo_egwas_tcx_data <- mayo_egwas_tcx_data[ind3[tcxSamples],]
+
 #write ids
-write.table(cere_cov[cere_keep,1:2],file='cerebellumIds.txt',sep=' ',quote=F,row.names=F,col.names=F)
-write.table(tcx_cov[tcx_keep,1:2],file='temporalcortexIds.txt',sep=' ',quote=F,row.names=F,col.names=F)
+write.table(cere_cov[,1:2],file='cerebellumIds.txt',sep=' ',quote=F,row.names=F,col.names=F)
+write.table(tcx_cov[,1:2],file='temporalcortexIds.txt',sep=' ',quote=F,row.names=F,col.names=F)
+
+
+#mayo_gwas_tcx_cov <- gwas_cov[gwas_cov$IID%in%tcx_cov$IID,]
 
 save.image(file='mayoEGWASdata.rda')
 
--- UFL-Mayo-ISB/loadEGWASdata.R
+++ UFL-Mayo-ISB/loadEGWASdata.R
@@ -4,9 +4,7 @@ synapseLogin()
 #load data
 load('mayoEGWASdata.rda')
 
-#read in expression data for cerebellum
-mayo_egwas_cere_data <- read.csv(egwas_cere@filePath,header=T)
-mayo_egwas_cere_data <- mayo_egwas_cere_data[cere_keep,]
+
 
 #read in cerebellum filtered genotype data for chromosome 22
 mayo_geno_cere_22 <- read.table('cerebellumGenotypes/AMP-AD_MayoLOADGWAS_UFL-Mayo-ISB_IlluminaHumanHap300_Cerebellum_22.raw',header=T)
