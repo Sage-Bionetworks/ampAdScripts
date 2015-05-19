bc0fdc4d093969e43d5a458ca295b1d7a8e07c3d
Ben Logsdon <aeberhart@gmail.com>
2015-05-19 10:51:17 -0700

    update to fxn to send messages

--- dataUpdateMessage.R
+++ dataUpdateMessage.R
@@ -1,7 +1,5 @@
 ###function to send data update messages
 dataUpdateMessage <- function(users,subject,message){
-  require(synapseClient)
-  synapseLogin()
   sendMessage(userIdList = users,subject = subject, body = message)
 }
 
