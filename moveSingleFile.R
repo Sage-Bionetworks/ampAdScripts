c707befc8b2b50cdb6284f48d7e14f8f74132a5c
Ben Logsdon <aeberhart@gmail.com>
2015-08-28 11:02:47 -0700

    function to move single file

---
+++ moveSingleFile.R
@@ -0,0 +1,8 @@
+moveSingleFile <- function(synId,newParentId){
+  #function to move a single file (synId) and place a link where it was given a new parentId (newParentId)
+  foo <- synGet(synId,downloadFile=F)
+  oldParentId <- synGetProperties(foo)$parentId
+  fileName <- synGetProperties(foo)$name
+  moveFile(synId,newParentId)
+  makeLink(fileName,synId,oldParentId)
+}
