55cf90850d4657cf424dc197b80b178b21843ced
Ben Logsdon <aeberhart@gmail.com>
2015-08-28 19:34:34 -0700

    fixed bugs

---
+++ RFunctions/makeLink.R
@@ -0,0 +1,14 @@
+makeLink <- function(fileName,fileId,parentId){
+  #fileName: name of file
+  #fileId: id of file
+  #parentId: parentId of where link goes
+  
+  require(rPython)
+  python.exec('import synapseclient');
+  python.exec('import json');
+  python.exec('syn = synapseclient.login()')
+  command <- paste("x = syn.restPOST(\'/entity\', json.dumps({\'name\':u\'",fileName,"\',\'linksTo\':{u\'targetId\': u\'",fileId,"\'},\'entityType\':u\'org.sagebionetworks.repo.model.Link\',\'linksToClassName\':u\'org.sagebionetworks.repo.model.FileEntity\',\'concreteType\':u\'org.sagebionetworks.repo.model.Link\',\'parentId\':u\'",parentId,"\'}))",sep='')
+  python.exec(command)
+}
+#syn3155374
+#syn3155376
