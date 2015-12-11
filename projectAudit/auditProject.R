599ea4860da88bc688e48f42b386fe0f4f60bf97
Benjamin Logsdon <aeberhart@gmail.com>
2015-12-11 14:37:39 -0800

    project audit, push full table to synapse

--- projectAudit/auditProject.R
+++ projectAudit/auditProject.R
@@ -65,10 +65,59 @@ fxn1 <- function(x){
 }
 entityType <- sapply(foo$type,fxn1)
 entityType[1] <- 'Project'
+entityType[2] <- 'Table'
 
 annotationAuditDataFrame <- data.frame(synId=foo$id,
                                        entityType=entityType,
-                                       hasAnnotation=annoExist)
+                                       hasAnnotation=annoExist,
+                                       stringsAsFactors = F)
+
+#anno2 <- lapply(anno,function(x){return(x[[1]])})
+fxn2 <- function(x){
+  bar <- NULL
+  try(bar <- names(x),silent=T)
+  return(bar)
+}
+fieldsAudit <- lapply(anno,fxn2)
+n1 <- names(dictionary)
+library(Hmisc)
+fieldAuditNew <- sapply(fieldsAudit,function(x,y){return(y%in%x)},n1)
+fieldAuditNew <- t(fieldAuditNew)
+colnames(fieldAuditNew) <- paste0('has',n1)
+
+annotationAuditDataFrame <- cbind(annotationAuditDataFrame,fieldAuditNew)
+
+#assayTargetValueInDictionary
+valueAuditNew <- fieldAuditNew
+colnames(valueAuditNew) <- paste0(n1,'ValueInDictionary')
+fxn3 <- function(x,n1,dictionary,i){
+  foobaz <- FALSE
+  try(foobaz <- x[[n1[i]]] %in% dictionary[[n1[i]]],silent=T)
+  if(length(foobaz)>0){
+    if(is.na(foobaz)){
+      foobaz <- FALSE
+    }
+  }else {
+    foobaz <- FALSE
+  }
+  return(foobaz[1])
+}
+
+for (i in 1:ncol(valueAuditNew)){
+  valueAuditNew[,i] <- sapply(anno,fxn3,n1,dictionary,i)
+}
+
+annotationAuditDataFrame <- cbind(annotationAuditDataFrame,valueAuditNew)
+colnames(annotationAuditDataFrame)[1] <- 'synapseID'
+tcresult<-as.tableColumns(annotationAuditDataFrame)
+cols<-tcresult$tableColumns
+fileHandleId<-tcresult$fileHandleId
+projectId<-"syn2397881"
+schema<-TableSchema(name="AMP AD Audit Full Table", parent=projectId, columns=cols)
+table<-Table(schema, fileHandleId)
+table<-synStore(table, retrieveData=TRUE)
+
+write.csv(annotationAuditDataFrame,file='annotationAuditDecember2015.csv',quote=F,row.names=F)
 
 ##cross reference annotations with dictionary
 ##return errors for cases where annotations and dictionary are inconsistent
