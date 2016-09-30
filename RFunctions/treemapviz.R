817be97e89bdc6a984bd5bc34f120ce5fb22ccb1
kdaily <kmdaily@gmail.com>
2016-09-30 15:17:54 -0700

    change query and plot

--- RFunctions/treemapviz.R
+++ RFunctions/treemapviz.R
@@ -4,10 +4,10 @@ library('tidyr')
 library('treemap')
 library('d3treeR')
 
-queryString <- "select study,center,platform,dataType,fileType,tissueType from file where projectId=='syn2580853'"
 library('synapseClient')
 synapseLogin()
 
+queryString <- "select center,assay,dataType,fileType from file where projectId=='syn2580853'"
 
 request <- synQuery(queryString, blockSize=250)
 queryResults <- request$collectAll()
@@ -21,6 +21,7 @@ d <- queryResults %>%
 tm <- treemap(d,
               index=c("center", "assay"),
               vSize="n",
-              type="value")
+              vColor=c("center"),
+              type="index", draw = FALSE)
 
-d3tree2(tm, rootname = "All Centers")
+d3tree2(tm, rootname = "All Centers", width='800', height='600')
