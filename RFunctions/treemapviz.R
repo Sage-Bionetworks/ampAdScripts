eef2a87768362a53670a0fda1a851473ae2b7896
kdaily <kdaily@users.noreply.github.com>
2016-09-30 10:13:57 -0700

    added assay filtering and drawing of plot

--- RFunctions/treemapviz.R
+++ RFunctions/treemapviz.R
@@ -1,16 +1,26 @@
-library('plyr')
 library('dplyr')
 library('reshape')
 library('tidyr')
-library('ggplot2')
 library('treemap')
 library('d3treeR')
 
 queryString <- "select study,center,platform,dataType,fileType,tissueType from file where projectId=='syn2580853'"
+library('synapseClient')
+synapseLogin()
+
 
 request <- synQuery(queryString, blockSize=250)
 queryResults <- request$collectAll()
 
 d <- queryResults %>% 
-  filter(file.dataType %in% c("mRNA", "miRNA", "DNA", "protein")) %>% 
-  count(file.center, file.dataType, sort = TRUE)
+  filter(!is.na(file.assay),
+         file.dataType %in% c("mRNA", "miRNA", "DNA", "protein")) %>% 
+  count(file.center, file.assay, sort = TRUE) %>% 
+  dplyr::rename(center=file.center, assay=file.assay)
+
+tm <- treemap(d,
+              index=c("center", "assay"),
+              vSize="n",
+              type="value")
+
+d3tree2(tm, rootname = "All Centers")
